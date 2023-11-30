26c940eb2f659bdb4610b315519f53da256849d6
pulkit-cse <101635335+pulkit-cse@users.noreply.github.com>
2023-11-30 12:29:53 +0530

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://pulkit1091:Pulkit0166@cluster0.as9rmgh.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
